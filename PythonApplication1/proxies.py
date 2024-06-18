1b0a09b5eefabee92b897dc5cc02bab97967597a
tshurlock <61873925+tshurlock@users.noreply.github.com>
2024-06-18 16:57:50 +0100

    updates to proxy getter to make class, need import for proxies

--- PythonApplication1/proxies.py
+++ PythonApplication1/proxies.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-import localValues
+from selenium.webdriver.chrome.service import Service
+import localVariables
+import time
+import proxyGetter
 
-
-proxies = ['189.240.60.168','172.183.241.1','189.240.60.163','221.140.235.236','203.205.9.105','85.111.60.196','221.140.235.237',
+'''proxies = ['189.240.60.168','172.183.241.1','189.240.60.163','221.140.235.236','203.205.9.105','85.111.60.196','221.140.235.237',
            '43.134.229.98','35.185.196.38','20.235.159.154','67.43.227.228','8.219.97.248','148.72.140.24','18.185.169.150',
            '3.127.62.252','15.236.145.64','3.37.125.76','18.228.198.164','3.122.84.99','52.16.232.164','52.67.10.183',
            '200.174.198.86','72.10.160.92','223.135.156.183','52.196.1.182','44.219.175.186','35.79.120.242','54.248.238.110',
@@ -20,17 +23,26 @@ proxies = ['189.240.60.168','172.183.241.1','189.240.60.163','221.140.235.236','
            '114.6.27.84','195.159.124.56','52.82.123.144','103.69.96.61','13.212.107.14','5.196.111.29','35.187.204.53',
            '41.111.243.133','8.213.151.128','5.189.184.6','34.215.74.117','20.219.176.57','185.141.134.120','62.210.37.74',
            '45.5.92.94','185.64.208.184','195.138.73.54','20.42.119.47','146.56.154.83','80.66.81.39','103.208.27.214',
-           '134.209.144.177','38.60.254.99',]
+           '134.209.144.177','38.60.254.99',]'''
 
+print('new go')
+driver = webDriver.Chrome() 
+free_proxies = proxyGetter.get_free_proxies(driver)
+print(free_proxies)
+'''
 for i in range(0, len(proxies)):
     try:
         print("Proxy selected: {}".format(proxies[i]))
         options = webdriver.ChromeOptions()
         options.add_argument('--proxy-server={}'.format(proxies[i]))
-        driver = webdriver.Chrome(options=options, executable_path= localValues.chromedriverPath)
+        driver = webdriver.Chrome( service=  Service(localVariables.chromedriverPath), 
+        options=options)
+        driver = webdriver.Chrome(options=options, executable_path= localVariables.chromedriverPath)
         driver.get("https://www.whatismyip.com/proxy-check/?iref=home")
+        time.sleep(10)
         if "Proxy Type" in WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "p.card-text"))):
             break
     except Exception:
         driver.quit()
 print("Proxy Invoked")
+'''
--- PythonApplication1/proxyGetter.py
+++ PythonApplication1/proxyGetter.py
@@ -1,33 +1,54 @@
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Chrome()
 
-# Get free proxies for rotating
-def get_free_proxies(driver):
-    driver.get('https://sslproxies.org')
 
-    table = driver.find_element(By.TAG_NAME, 'table')
-    thead = table.find_element(By.TAG_NAME, 'thead').find_elements(By.TAG_NAME, 'th')
-    tbody = table.find_element(By.TAG_NAME, 'tbody').find_elements(By.TAG_NAME, 'tr')
 
-    headers = []
-    for th in thead:
-        headers.append(th.text.strip())
+class cheese:
+    def __init__(self) -> None:
+        self.driver = webdriver.Chrome()
 
-    proxies = []
-    for tr in tbody:
-        proxy_data = {}
-        tds = tr.find_elements(By.TAG_NAME, 'td')
-        for i in range(len(headers)):
-            proxy_data[headers[i]] = tds[i].text.strip()
-        proxies.append(proxy_data)
+    def get_free_proxies(driver):
+        driver = webdriver.Chrome()
+        driver.get('https://sslproxies.org')
+
+        table = driver.find_element(By.TAG_NAME, 'table')
+        thead = table.find_element(By.TAG_NAME, 'thead').find_elements(By.TAG_NAME, 'th')
+        tbody = table.find_element(By.TAG_NAME, 'tbody').find_elements(By.TAG_NAME, 'tr')
+
+        headers = []
+        for th in thead:
+            headers.append(th.text.strip())
+
+        proxies = []
+        for tr in tbody:
+            proxy_data = {}
+            tds = tr.find_elements(By.TAG_NAME, 'td')
+            for i in range(len(headers)):
+                proxy_data[headers[i]] = tds[i].text.strip()
+            proxies.append(proxy_data)
     
-    return proxies
+        return proxies
+
+    def extract_ip_addresses(self, ip_list):
+    # Initialize an empty list to store the extracted IP addresses
+        ip_addresses = []
+    
+    # Iterate over each dictionary in the list
+        for entry in ip_list:
+        # Extract the 'IP Address' value from the dictionary
+            ip = entry.get('IP Address')
+            if ip:
+                ip_addresses.append(ip)
+    
+        return ip_addresses
 
 
-free_proxies = get_free_proxies(driver)
+p1= cheese()
 
-print(free_proxies)
+
+a1 = p1.get_free_proxies()
+b1 = p1.extract_ip_addresses(a1)
+print(b1)
